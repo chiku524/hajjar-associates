2c43f16d35462cba3c96c69d85d1344f5792e816
Nico Chikuji <chiku524@icloud.com>
2020-11-05 02:58:21 -0500

    trying the zoom out for mobile

--- website/src/components/ScrollToTop.js
+++ website/src/components/ScrollToTop.js
@@ -1,11 +1,20 @@
 import React, { useEffect, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
 
+function zoomOutMobile() {
+  var viewport = document.querySelector('meta[name="viewport"]');
+
+  if ( viewport ) {
+    viewport.content = "initial-scale=0.1";
+    viewport.content = "width=1200";
+  }
+}
+
 function ScrollToTop({ history, children }) {
   useEffect(() => {
     const unlisten = history.listen(() => {
       window.scrollTo(0, 0);
-      document.body.style.tranform = 'scale(.8)';
+      zoomOutMobile();
     });
     return () => {
       unlisten();
