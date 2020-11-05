00b75dc9761e954c18c4d745528dacf4d6e75993
Nico Chikuji <chiku524@icloud.com>
2020-11-05 03:28:03 -0500

    tried making width 100% for mobile

--- website/src/components/ScrollToTop.js
+++ website/src/components/ScrollToTop.js
@@ -5,8 +5,6 @@ function ScrollToTop({ history, children }) {
   useEffect(() => {
     const unlisten = history.listen(() => {
         window.scrollTo(0, 0);
-        let viewportmeta = document.querySelector('meta[name="viewport"]');
-        viewportmeta.setAttribute('content', "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0");
     });
     return () => {
       unlisten();
